3a77d48ce1206a6bec5d5a6c49f792b3546b2ed3
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2024-06-10 01:43:09 +0400

    Create Положительные числа.py

---
+++ Branching/Положительные числа.py
@@ -0,0 +1,21 @@
+a = int(input())
+b = int(input())
+c = int(input())
+summ1 = 0
+summ2 = 0
+summ3 = 0
+
+if (a > 0):
+    summ1 = a
+else:
+    print("Число а - отрицательное")
+if(b > 0):
+    summ2 =  b
+else:
+    print("Число б отрицательное")
+if (c > 0):
+    summ3 = c
+else:
+    print("Число с отрицательное")
+
+print(summ1 + summ2 + summ3)
